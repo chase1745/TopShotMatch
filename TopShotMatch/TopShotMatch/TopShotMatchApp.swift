2f860d2ad4cba087214b13ef72a38d85ca0e51e7
Chase McDermott <chase.mcdermott12@gmail.com>
2023-02-20 16:08:47 -0600

    Initial commit 🏀

---
+++ TopShotMatch/TopShotMatch/TopShotMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopShotMatchApp.swift
+//  TopShotMatch
+//
+//  Created by Chase McDermott on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct TopShotMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
